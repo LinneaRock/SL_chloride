5d6ceb12be855a47265a6ffb186317c0571efd51
LinneaRock <lrock@wisc.edu>
2020-07-31 12:30:40 -0500

    mapping not working

---
+++ LAGOS.R
@@ -0,0 +1,43 @@
+library(tidyverse)
+library(sf)
+library(LAGOSNE)
+library(ggspatial)
+
+
+lagos <- lagosne_load()
+names(lagos)
+
+info_SL <- lake_info(name = "Sparkling", state = "Wisconsin")
+locus <- lagos$locus %>%
+  filter(lagoslakeid == 906)
+geo <- lagos$lakes.geo %>%
+  filter(lagoslakeid == 906)
+lulc <- lagos$iws.lulc %>%
+  filter(lagoslakeid == 906)
+limno <- lagos$lakes_limno %>%
+  filter(lagoslakeid == 906)
+
+#iws.sf <- st_read("C:/Users/linne/Downloads/IWS/IWS.shp")
+#iws.sf.SL <- iws.sf %>%
+#  filter(ZoneID == "IWS_29670")
+
+#write_rds(iws.sf.SL, "Data/shapefiles/IWS.rds")
+
+iws.sf.SL <- read_rds("Data/shapefiles/IWS.rds")
+
+#esri_land <-  paste0('https://services.arcgisonline.com/arcgis/rest/services/NatGeo_World_Map/MapServer/tile/${z}/${y}/${x}.jpeg')
+world_gray <- paste0('https://services.arcgisonline.com/arcgis/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/${z}/${y}/${x}.jpeg')
+
+
+ggplot() + 
+  annotation_map_tile(type = world_gray, zoom = 12) + # Esri Basemap (zoom sets level of detail, higher = higherRes)
+  geom_sf(data = iws.sf.SL) + 
+  theme_bw() + # Hilary's default theme
+  theme(legend.position = c(0.9,0.85)) +
+  annotation_scale(location = "br", width_hint = 0.5,height = unit(0.05,'in')) + # Scale bar
+  annotation_north_arrow(location = "bl", which_north = "true", 
+                         # pad_x = unit(0.2, "in"), pad_y = unit(0.2, "in"),
+                         height = unit(0.5,'in'), width = unit(0.5,'in'),
+                         style = north_arrow_nautical) + # North Arrow
+  coord_sf(datum = NA, ylim = c(46.0, 46.2), xlim = c(-89.69, -89.55), expand = FALSE) # limit axes
+
